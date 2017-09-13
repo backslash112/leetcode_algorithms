cf4b06dcb7e0b1fa63393e61ac8354cc25be24a3
backslash112 <yangcun@live.com>
2017-09-13 11:30:53 +0800

    Fix a bug: The result bigger that the max int value. -> Using long instend int.

--- reverse_integer.java
+++ reverse_integer.java
@@ -4,24 +4,25 @@ import java.util.Arrays;
 public class reverse_integer {
     
     public static void main(String[] args) {
-        // System.out.println(reverseInteger(123));
-        reverseInteger2(123);
+        System.out.println(reverseInteger(1534236469));
+        // reverseInteger2(123);
     }
 
     public static int reverseInteger(int i) {
-        int inputLong = i;
+        long inputLong = i;
         System.out.println(String.format("inputLong: %d", inputLong));
-        int reversedNum = 0;
+        long reversedNum = 0;
         while(inputLong != 0) {
             System.out.println(String.format("----- \r\n reversedNum: %d = %d x 10 + %d %% 10", reversedNum * 10 + inputLong % 10, reversedNum * 10, inputLong % 10));
             reversedNum = reversedNum * 10 + inputLong % 10;
             inputLong = inputLong / 10;
             System.out.println(String.format("inputLong: %d", inputLong));
         }
+        System.out.println(Integer.MAX_VALUE);
         if (reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE) {
             throw new IllegalArgumentException();
         }
-        return reversedNum;
+        return (int)reversedNum;
     }
 
     public static void reverseInteger2(int input) {
@@ -32,5 +33,4 @@ public class reverse_integer {
             input = input / 10;
         }
     }
-
 }
